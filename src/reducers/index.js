1d7c47bbb740ee86546b6b9e8f70b5e25babab78
sparebeat0814 <cannonball4114@gmail.com>
2022-04-19 17:59:02 -0600

    setup redux devtools fixed reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 
-import { reducer as form } from 'redux-form'
+import { reducer as form } from 'redux-form';
+
+import posts from "./postsReducer";
 
 const rootReducer = combineReducers({
-  form
+  form,
+  posts
 });
 
 export default rootReducer;
