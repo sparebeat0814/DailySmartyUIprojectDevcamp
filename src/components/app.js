a0445e0d67c1326325d9db369a5f9cfe27f340a4
sparebeat0814 <cannonball4114@gmail.com>
2022-04-18 20:35:17 -0600

    logo is all setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div>
-          Logo
+          <Logo/>
         </div>
       </div>
     )}
