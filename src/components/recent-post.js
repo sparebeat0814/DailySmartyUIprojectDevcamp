f0bb68f8f75e037174c46aa8af7dd1517e934e00
sparebeat0814 <cannonball4114@gmail.com>
2022-04-19 18:36:44 -0600

    recnt post component

--- src/components/recent-post.js
+++ src/components/recent-post.js
@@ -11,15 +11,26 @@ class RecentPosts extends Component {
         this.props.fetchRecentPosts();
     }
 
+    renderPosts = function() {
+        const posts = this.props.recentPosts.map((post, index) => {
+            if(index < 3) {
+            return (
+                <li key={index}>
+                    {posts.title}
+                </li>
+            )
+        }
+        })
+        return posts
+    }
+
     render() {
         return (
             <div className='recent-posts'>
                 <div className='recent_posts_wrapper'>
                     <div className='recent_posts_heading'>Recent Posts</div>
                     <ul className='recent-posts__posts'>
-                         <li>Recent post 0</li>
-                         <li>Recent post 1</li>
-                         <li>Recent post 2</li>
+                         {this.renderPosts()}
                     </ul>
                 </div>
 
@@ -28,4 +39,10 @@ class RecentPosts extends Component {
  }
 }
 
-export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+    return {
+    recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
